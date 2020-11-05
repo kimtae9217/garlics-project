777d699e2e1fadea1821be15409aeb43c81e43ed
taewon <kimtae9217@gmail.com>
2020-11-05 17:50:47 +0900

    timer animation

--- app/src/main/java/com/example/myapplication/Timer.java
+++ app/src/main/java/com/example/myapplication/Timer.java
@@ -1,13 +1,18 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Chronometer;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,8 +20,9 @@ import androidx.fragment.app.Fragment;
 
 public class Timer extends Fragment {
     Chronometer chronometer;
-    ImageButton btStart, btStop;
-
+    ImageButton btStart, btStop, btPause;
+    ImageView icanchor;
+    Animation roundingalone;
     private boolean isResume;
     Handler handler;
     long tMilliSec, tStart, tBuff, tUpadate = 0L;
@@ -27,80 +33,81 @@ public class Timer extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.timer, container, false);
-        return viewGroup;
 
-    }
+        chronometer = (Chronometer)viewGroup.findViewById(R.id.chronometer);
+        btStart = (ImageButton) viewGroup.findViewById(R.id.bt_start);
+        btPause = (ImageButton)viewGroup.findViewById(R.id.bt_stop);
+        btStop = (ImageButton)viewGroup.findViewById(R.id.bt_stop);
+        icanchor = (ImageView)viewGroup.findViewById(R.id.icanchor);
 
-    class test extends AppCompatActivity {
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.timer);
+        roundingalone = AnimationUtils.loadAnimation(getActivity(), R.anim.roundingalone);
 
-            chronometer = findViewById(R.id.chronometer);
-            btStart = findViewById(R.id.bt_start);
-            btStop = findViewById(R.id.bt_stop);
+        handler = new Handler();
 
-            handler = new Handler();
-
-            btStart.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(!isResume){
-                        tStart = SystemClock.uptimeMillis();
-                        handler.postDelayed(runnable, 0);
-                        chronometer.start();
-                        isResume = true;
-                        btStop.setVisibility(View.GONE);
-                        btStart.setImageDrawable(getResources().getDrawable(
-                                R.drawable.ic_pause
-                        ));
-                    }else {
-                        tBuff += tMilliSec;
-                        handler.removeCallbacks(runnable);
-                        chronometer.stop();
-                        isResume = false;
-                        btStop.setVisibility(View.VISIBLE);
-                        btStart.setImageDrawable(getResources().getDrawable(
-                                R.drawable.ic_play
-                        ));
-                    }
-                }
-            });
-
-            btStop.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (!isResume){
-                        btStart.setImageDrawable(getResources().getDrawable(
-                                R.drawable.ic_play
-                        ));
-                        tMilliSec = 0L;
-                        tStart = 0L;
-                        tBuff = 0L;
-                        tUpadate = 0L;
-                        sec = 0;
-                        min = 0;
-                        milliSec = 0;
-                        chronometer.setText("00:00:00");
-
-                    }
-                }
-            });
-        }
-        public  Runnable runnable = new Runnable() {
+        btStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                tMilliSec = SystemClock.uptimeMillis() - tStart;
-                tUpadate = tBuff + tMilliSec;
-                sec = (int) (tUpadate/1000);
-                min = sec/60;
-                sec = sec%60;
-                milliSec = (int) (tUpadate%100);
-                chronometer.setText(String.format("%02d", min)+":"+String.format("%02d",sec)+":"+String.format("%02d",milliSec));
-                handler.postDelayed(this, 60);
-            }
-        };
-    }
-}
+            public void onClick(View v) {
+                icanchor.startAnimation(roundingalone); //Animation Start
 
+                if(!isResume){
+                    tStart = SystemClock.uptimeMillis();
+                    handler.postDelayed(runnable, 0);
+                    chronometer.start();
+                    isResume = true;
+                    btStop.setVisibility(View.GONE);
+//                    btPause.setVisibility(View.GONE);
+                    btStart.setImageDrawable(getResources().getDrawable(
+                            R.drawable.ic_pause
+                    ));
+                }else {
+                    icanchor.getAnimation().cancel();
+                    tBuff += tMilliSec;
+                    handler.removeCallbacks(runnable);
+                    chronometer.stop();
+                    isResume = false;
+                    btStop.setVisibility(View.VISIBLE);
+                    btStart.setImageDrawable(getResources().getDrawable(
+                            R.drawable.ic_play
+                    ));
+                }
+            }
+        });
+
+        btStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                icanchor.clearAnimation(); //Animation Stop
+                if (!isResume){
+                    btStart.setImageDrawable(getResources().getDrawable(
+                            R.drawable.ic_play
+                    ));
+                    tMilliSec = 0L;
+                    tStart = 0L;
+                    tBuff = 0L;
+                    tUpadate = 0L;
+                    sec = 0;
+                    min = 0;
+                    milliSec = 0;
+                    chronometer.setText("00:00:00");
+
+                }
+            }
+        });
+        return viewGroup;
+    }
+
+    public  Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            tMilliSec = SystemClock.uptimeMillis() - tStart;
+            tUpadate = tBuff + tMilliSec;
+            sec = (int) (tUpadate/1000);
+            min = sec/60;
+            sec = sec%60;
+            milliSec = (int) (tUpadate%100);
+            chronometer.setText(String.format("%02d", min)+":"+String.format("%02d",sec)+":"+String.format("%02d",milliSec));
+            handler.postDelayed(this, 60);
+        }
+    };
+}
 
