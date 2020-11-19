66cde33912dba063b70116b2115bd6bf6f24a943
shin <rkrkdldkd@naver.com>
2020-11-19 17:28:22 +0900

    2020. 11. 19 merge!

--- app/src/main/java/com/example/myapplication/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,11 +18,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Profile extends Fragment  {
+public class Profile extends Fragment {
 
-    private static final int REQUEST_CODE = 0;
-    ImageView userProfile, morning;
+    public static final int USER_PROFILE = 0;
+    public static final int MORNING = 0;
+    ImageView userProfile, morning, lunch, dinner, snack;
     ViewGroup viewGroup;
+    Bitmap bm;
+    TextView tx;
 
     @Nullable
     @Override
@@ -29,57 +33,96 @@ public class Profile extends Fragment  {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.profile, container, false);
         userProfile = (ImageView) viewGroup.findViewById(R.id.profile_userPicture);
         morning = (ImageView) viewGroup.findViewById(R.id.profile_morning_image);
+        lunch = (ImageView) viewGroup.findViewById(R.id.profile_lunch_image);
+        dinner = (ImageView) viewGroup.findViewById(R.id.profile_dinner_Image);
+        snack = (ImageView) viewGroup.findViewById(R.id.profile_snack_image);
 
 
 
         userProfile.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-            {
-                doTakeAlbumAction();
+                {
+
+                    doTakeAlbumAction(USER_PROFILE);
+
+
                 }
-
             }
-
         });
+
         morning.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getActivity(),Enroll_photo.class );
+                Intent intent = new Intent(getActivity(), Enroll_photo.class);
+                startActivityForResult(intent, MORNING);
+
+            }
+        });
+
+        lunch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Enroll_photo.class);
+                startActivity(intent);
+
+            }
+        });
+
+        dinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Enroll_photo.class);
                 startActivity(intent);
             }
         });
 
+        snack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Enroll_photo.class);
+                startActivity(intent);
+            }
+        });
         return viewGroup;
     }
 
-    public void doTakeAlbumAction() {
+    public void doTakeAlbumAction(int num) {
 
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, REQUEST_CODE);
-
+        startActivityForResult(intent, num);
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        InputStream is = null;
-        try {
-            is = getActivity().getContentResolver().openInputStream(data.getData());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        Bitmap bm = BitmapFactory.decodeStream(is);
-        try {
-            is.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        userProfile.setImageBitmap(bm);
 
-    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == USER_PROFILE  )
+              {
+                InputStream is = null;
+                try {
+                    is = getActivity().getContentResolver().openInputStream(data.getData());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                 bm = BitmapFactory.decodeStream(is);
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
+                userProfile.setImageBitmap(bm);
+
+            }else if(requestCode == MORNING){
+                Bundle bundle = getArguments();
+                morning.setImageBitmap(bundle.getParcelable("image"));
+            }
+
+            }
 
 }
+
+
+
 
--- app/src/main/java/com/example/myapplication/SetPhoto.java
+++ app/src/main/java/com/example/myapplication/SetPhoto.java
@@ -1,279 +1,77 @@
 package com.example.myapplication;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
-public class SetPhoto extends Activity implements OnClickListener
-
-{
-
-    private static final int PICK_FROM_CAMERA = 0;
-    private static final int PICK_FROM_ALBUM = 1;
-    private static final int CROP_FROM_CAMERA = 2;
-
-    private Uri mImageCaptureUri;
-    private ImageView mPhotoImageView;
-
-    /**
-
-     * 카메라에서 이미지 가져오기
-
-     */
-
-    public void doTakePhotoAction()
-
-    {
-
-
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        // 임시로 사용할 파일의 경로를 생성
-
-        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
-
-        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
-
-
-        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
-
-        // 특정기기에서 사진을 저장못하는 문제가 있어 다음을 주석처리 합니다.
-
-        //intent.putExtra("return-data", true);
-
-        startActivityForResult(intent, PICK_FROM_CAMERA);
-
-    }
-
-
-
-    /**
-
-     * 앨범에서 이미지 가져오기
-
-     */
-
-    public void doTakeAlbumAction()
-
-    {
-
-        // 앨범 호출
-
-        Intent intent = new Intent(Intent.ACTION_PICK);
-
-        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-
-        startActivityForResult(intent, PICK_FROM_ALBUM);
-
-    }
-
+public class SetPhoto extends Activity {
+    ImageView userProfile, morning, lunch, dinner, snack;
+    TextView tx;
+    Bitmap bm;
+    Uri uri;
 
 
     @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+        tx = (TextView)findViewById(R.id.textView123) ;
 
-    {
+        Intent intent = getIntent();
 
-        if(resultCode != RESULT_OK)
-
-        {
-
-            return;
-
-        }
-
-
-
-        switch(requestCode)
-
-        {
-
-            case CROP_FROM_CAMERA:
-
-            {
-
-                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
-
-                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
-
-                // 임시 파일을 삭제합니다.
-
-                final Bundle extras = data.getExtras();
-
-
-
-                if(extras != null)
-
-                {
-
-                    Bitmap photo = extras.getParcelable("data");
-
-                    mPhotoImageView.setImageBitmap(photo);
-
-                }
-
-
-
-                // 임시 파일 삭제
-
-                File f = new File(mImageCaptureUri.getPath());
-
-                if(f.exists())
-
-                {
-
-                    f.delete();
-
-                }
-
-
-
-                break;
-
-            }
-
-
-
-            case PICK_FROM_ALBUM:
-
-            {
-
-                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
-
-                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
-
-
-
-                mImageCaptureUri = data.getData();
-
-            }
-
-
-
-            case PICK_FROM_CAMERA:
-
-            {
-
-                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
-
-                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
-
-
-
-                Intent intent = new Intent("com.android.camera.action.CROP");
-
-                intent.setDataAndType(mImageCaptureUri, "image/*");
-
-
-
-                intent.putExtra("outputX", 90);
-
-                intent.putExtra("outputY", 90);
-
-                intent.putExtra("aspectX", 1);
-
-                intent.putExtra("aspectY", 1);
-
-                intent.putExtra("scale", true);
-
-                intent.putExtra("return-data", true);
-
-                startActivityForResult(intent, CROP_FROM_CAMERA);
-
-
-
-                break;
-
-            }
-
+        if (intent.getIntExtra("profile", 1) == 1) {
+            doTakeAlbumAction(1);
+            intent.putExtra("image",uri);
+            setResult(RESULT_OK,intent);
+            finish();
         }
 
     }
 
 
+    public void doTakeAlbumAction(int num) {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, num);
+    }
+
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 ) {
 
-    public void onClick(View v)
 
-    {
+            InputStream is = null;
+            try { is = getContentResolver().openInputStream(data.getData());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            bm = BitmapFactory.decodeStream(is);
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            userProfile.setImageBitmap(bm);
+            uri = data.getData();
 
-        DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener()
-
-        {
-
-            @Override
-
-            public void onClick(DialogInterface dialog, int which)
-
-            {
-
-                doTakePhotoAction();
+            } else if (requestCode == 2) {
 
             }
-
-        };
-
-
-
-        DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener()
-
-        {
-
-            @Override
-
-            public void onClick(DialogInterface dialog, int which)
-
-            {
-
-                doTakeAlbumAction();
-
-            }
-
-        };
+        }
+    }
 
 
 
-        DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener()
 
-        {
-
-            @Override
-
-            public void onClick(DialogInterface dialog, int which)
-
-            {
-
-                dialog.dismiss();
-
-            }
-
-        };
-
-
-
-        new AlertDialog.Builder(this)
-
-                .setTitle("업로드할 이미지 선택")
-
-                .setPositiveButton("사진촬영", cameraListener)
-
-                .setNeutralButton("앨범선택", albumListener)
-
-                .setNegativeButton("취소", cancelListener)
-
-                .show();
-
-    }}
